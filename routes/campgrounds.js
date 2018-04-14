f6270950706fec8b2ec946e9e3035eb3eeda637c
Tyler <tyler.r.fisher@icloud.com>
2018-04-14 18:07:25 +0000

    Create EDIT and UPDATE routes and edit view. Update SHOW view to add edit button. Also install method-override node.

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -59,6 +59,31 @@ router.get("/:id", function(req, res){
   });
 });
 
+// EDIT - Show edit form for specified campground
+router.get("/:id/edit", function(req, res){
+  Campground.findById(req.params.id, function(err, foundCampground){
+    if (err) {
+      console.log(err);
+      res.redirect("/campgrounds")
+    } else {
+      res.render("campgrounds/edit", {campground: foundCampground});
+    }
+  });
+});
+
+// UPDATE - Update specified campground
+router.put("/:id", function(req, res){
+  // find and update campground
+  Campground.findByIdAndUpdate(req.params.id, req.body.campground, function(err, updatedCampground){
+    if (err) {
+      console.log(err);
+      res.redirect("/campgrounds");
+    } else {
+      res.redirect("/campgrounds/" + req.params.id);
+    }
+  });
+});
+
 ////////////////
 // Middleware //
 ////////////////
