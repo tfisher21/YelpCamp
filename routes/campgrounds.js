9b727c342c0d3462a86398333fbd412f87b88800
Tyler <tyler.r.fisher@icloud.com>
2018-04-15 21:12:19 +0000

    Create checkCampgroundOwnership middleware and apply to EDIT route

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -60,14 +60,9 @@ router.get("/:id", function(req, res){
 });
 
 // EDIT - Show edit form for specified campground
-router.get("/:id/edit", function(req, res){
+router.get("/:id/edit", checkCampgroundOwnership, function(req, res){
   Campground.findById(req.params.id, function(err, foundCampground){
-    if (err) {
-      console.log(err);
-      res.redirect("/campgrounds")
-    } else {
-      res.render("campgrounds/edit", {campground: foundCampground});
-    }
+    res.render("campgrounds/edit", {campground: foundCampground});
   });
 });
 
@@ -107,6 +102,26 @@ function isLoggedIn(req, res, next){
   res.redirect("/login");
 }
 
+function checkCampgroundOwnership(req, res, next) {
+  if (req.isAuthenticated()) {
+    Campground.findById(req.params.id, function(err, foundCampground){
+      if (err) {
+        console.log(err);
+        res.redirect("/campgrounds")
+      } else {
+        // Does User hold campground
+        if (foundCampground.author.id.equals(req.user._id)) {
+          next
+        } else {
+          res.send("You do not have permission to do that!");
+        }
+      }
+    });
+  } else {
+    console.log("You must be logged in to do that!");
+    res.redirect("back");
+  }
+}
 //////////////////////////////
 
 module.exports = router;
