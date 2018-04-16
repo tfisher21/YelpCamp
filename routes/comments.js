12a08a8f51a63eacb341ffc7e559459ed60e1783
Tyler <tyler.r.fisher@icloud.com>
2018-04-16 03:01:03 +0000

    Add EDIT and UPDATE routes. Add Edit view.

--- routes/comments.js
+++ routes/comments.js
@@ -49,6 +49,30 @@ router.post("/", function(req, res){
   });
 });
 
+// EDIT - Show form to edit a comment
+router.get("/:comment_id/edit", function(req, res){
+  Comment.findById(req.params.comment_id, function(err, foundComment){
+    if (err) {
+      console.log(err);
+      res.redirect("back");
+    } else {
+      res.render("comments/edit", {campground_id: req.params.id, comment: foundComment});
+    }
+  });
+});
+
+// UPDATE - Update comment in database
+router.put("/:comment_id", function(req, res){
+  Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function(err, updatedComment){
+    if (err) {
+      console.log(err);
+      res.redirect("back");
+    } else {
+      res.redirect("/campgrounds/" + req.params.id);
+    }
+  });
+});
+
 ////////////////
 // Middleware //
 ////////////////
